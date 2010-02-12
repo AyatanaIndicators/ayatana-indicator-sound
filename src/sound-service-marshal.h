b11795d4a1b9bfa79ecaaca0ce4943e2f4f3e4a7
Ken VanDine <ken.vandine@canonical.com>
2010-02-11 23:15:02 -0500

    merged upstream tarball

---
+++ src/sound-service-marshal.h
@@ -0,0 +1,20 @@
+
+#ifndef ___sound_service_marshal_MARSHAL_H__
+#define ___sound_service_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:INT,BOOLEAN (./sound-service.list:1) */
+extern void _sound_service_marshal_VOID__INT_BOOLEAN (GClosure     *closure,
+                                                      GValue       *return_value,
+                                                      guint         n_param_values,
+                                                      const GValue *param_values,
+                                                      gpointer      invocation_hint,
+                                                      gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* ___sound_service_marshal_MARSHAL_H__ */
+
