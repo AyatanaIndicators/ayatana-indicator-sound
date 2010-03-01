7a92d249547ff1783725850edb39a952e52ee8c9
Ted Gould <ted@gould.cx>
2010-03-01 16:06:52 -0600

    Removing including a C file and including some needed headers.

--- tests/test-indicator-sound.c
+++ tests/test-indicator-sound.c
@@ -18,7 +18,8 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <string.h>
-#include "../src/indicator-sound.c"
+#include <gtk/gtk.h>
+#include <libindicator/indicator-object.h>
 
 
 void test_libindicator_sound_init()
