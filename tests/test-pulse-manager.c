2e85d8a7db825a7aef087710abe8159a716df6c7
Robert Collins <robertc@robertcollins.net>
2010-03-09 09:24:36 +1100

    snapshot closer-to-building.

--- tests/test-pulse-manager.c
+++ tests/test-pulse-manager.c
@@ -1,6 +1,25 @@
-#include "defintions.h"
-// I know this is not nice but I would rather not make everything public ?
-#include "pulse-manager.c"
+/*
+Copyright 2010 Canonical Ltd.
+
+Authors:
+    Robert Collins <robert.collins@canonical.com>
+    Conor Curran <conor.curran@canonical.com>
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License version 3, as published 
+by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranties of 
+MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* we test static functions */
+#include "../src/pulse-manager.c"
 
 pa_context* context;
 
