a0c3edbbe3d6cbb160049716cec66bf2673784e4
Conor Curran <conor.curran@canonical.com>
2010-06-28 15:56:39 +0100

    bumped the revision no. and changed credits on sound-service

--- src/sound-service.c
+++ src/sound-service.c
@@ -4,9 +4,7 @@ Copyright 2010 Canonical Ltd.
 
 Authors:
     Conor Curran <conor.curran@canonical.com>
-    Ted Gould <ted@canonical.com>
-    Cody Russell <crussell@canonical.com>
-
+    
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License version 3, as published
 by the Free Software Foundation.
