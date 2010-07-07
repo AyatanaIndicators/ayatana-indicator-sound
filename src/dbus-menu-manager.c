6eef19e54ead5184d2cb136d3e6fdacc6eef37bf
Conor Curran <conor.curran@canonical.com>
2010-07-07 11:51:47 +0100

    typo in merge

--- src/dbus-menu-manager.c
+++ src/dbus-menu-manager.c
@@ -174,6 +174,7 @@ static void show_sound_settings_dialog (DbusmenuMenuitem *mi, gpointer user_data
   GError * error = NULL;
   if (!g_spawn_command_line_async("gnome-volume-control", &error) &&
       !g_spawn_command_line_async("xfce4-mixer", &error)) 
+  {
     g_warning("Unable to show dialog: %s", error->message);
     g_error_free(error);
   }
