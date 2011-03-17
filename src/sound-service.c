09ac621b4caf975b36433bdb0f053317c960223e
Conor Curran <conor.curran@canonical.com>
2011-03-17 16:10:03 +0000

    working now - needed a hack in the common-def header - automake you drive me nuts

--- src/sound-service.c
+++ src/sound-service.c
@@ -55,7 +55,6 @@ main (int argc, char ** argv)
   g_type_init();
   textdomain (GETTEXT_PACKAGE);
   bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
-  bind_text_domain_codeset (GETTEXT_PACKAGE,  "UTF-8");
   setlocale (LC_ALL, "");
 
   IndicatorService *service = indicator_service_new_version(INDICATOR_SOUND_DBUS_NAME,
