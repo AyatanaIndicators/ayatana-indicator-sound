858d8ee663212c5efa4561b0a2abb9c1d2485c27
Ken VanDine <ken.vandine@canonical.com>
2012-03-19 10:58:53 -0400

    Import upstream version 0.8.4.1

--- src/metadata-widget.c
+++ src/metadata-widget.c
@@ -22,7 +22,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config.h"
 #endif
 
-#include <glib/gi18n.h>
+#include <glib/gi18n-lib.h>
 #include "metadata-widget.h"
 #include "common-defs.h"
 #include <gtk/gtk.h>
