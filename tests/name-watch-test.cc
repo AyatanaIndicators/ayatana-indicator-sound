868453ef75110badecdb54e13b79a74cfc95282a
Ted Gould <ted@gould.cx>
2013-11-15 16:34:20 -0600

    Woot, a test, kinda

---
+++ tests/name-watch-test.cc
@@ -0,0 +1,21 @@
+
+#include <gio/gio.h>
+#include <gtest/gtest.h>
+
+class NameWatchTest : public ::testing::Test
+{
+	protected:
+		virtual void SetUp() {
+
+		}
+
+		virtual void TearDown() {
+
+		}
+};
+
+TEST_F(NameWatchTest, DummyTest)
+{
+
+
+}
