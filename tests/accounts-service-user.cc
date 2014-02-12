073c7cefb2ae5aaae12e2251523732034e3b2f4b
Ted Gould <ted@gould.cx>
2014-02-11 22:19:03 -0600

    Drop a dbus mock on this problem

--- tests/accounts-service-user.cc
+++ tests/accounts-service-user.cc
@@ -31,12 +31,66 @@ class AccountsServiceUserTest : public ::testing::Test
 
 	protected:
 		DbusTestService * service = NULL;
+		DbusTestDbusMock * mock = NULL;
 
 		GDBusConnection * session = NULL;
 		GDBusConnection * system = NULL;
 
 		virtual void SetUp() {
 			service = dbus_test_service_new(NULL);
+
+			mock = dbus_test_dbus_mock_new("org.freedesktop.Accounts");
+
+			DbusTestDbusMockObject * baseobj = dbus_test_dbus_mock_get_object(mock, "/org/freedesktop/Accounts", "org.freedesktop.Accounts", NULL);
+
+			dbus_test_dbus_mock_object_add_method(mock, baseobj,
+				"CacheUser", G_VARIANT_TYPE_STRING, G_VARIANT_TYPE_OBJECT_PATH,
+				"ret = dbus.ObjectPath('/user')\n", NULL);
+			dbus_test_dbus_mock_object_add_method(mock, baseobj,
+				"FindUserById", G_VARIANT_TYPE_INT64, G_VARIANT_TYPE_OBJECT_PATH,
+				"ret = dbus.ObjectPath('/user')\n", NULL);
+			dbus_test_dbus_mock_object_add_method(mock, baseobj,
+				"FindUserByName", G_VARIANT_TYPE_STRING, G_VARIANT_TYPE_OBJECT_PATH,
+				"ret = dbus.ObjectPath('/user')\n", NULL);
+			dbus_test_dbus_mock_object_add_method(mock, baseobj,
+				"ListCachedUsers", NULL, G_VARIANT_TYPE_OBJECT_PATH_ARRAY,
+				"ret = [ dbus.ObjectPath('/user') ]\n", NULL);
+			dbus_test_dbus_mock_object_add_method(mock, baseobj,
+				"UncacheUser", G_VARIANT_TYPE_STRING, NULL,
+				"", NULL);
+
+			DbusTestDbusMockObject * userobj = dbus_test_dbus_mock_get_object(mock, "/user", "org.freedesktop.Accounts.User", NULL);
+			dbus_test_dbus_mock_object_add_property(mock, userobj,
+				"UserName", G_VARIANT_TYPE_STRING,
+				g_variant_new_string(g_get_user_name()), NULL);
+
+			DbusTestDbusMockObject * soundobj = dbus_test_dbus_mock_get_object(mock, "/user", "com.canonical.indicator.sound.AccountsService", NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"PlayerName", G_VARIANT_TYPE_STRING,
+				g_variant_new_string(""), NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"PlayerIcon", G_VARIANT_TYPE_VARIANT,
+				g_variant_new_variant(g_variant_new_string("")), NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"Running", G_VARIANT_TYPE_BOOLEAN,
+				g_variant_new_boolean(FALSE), NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"State", G_VARIANT_TYPE_STRING,
+				g_variant_new_string(""), NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"Title", G_VARIANT_TYPE_STRING,
+				g_variant_new_string(""), NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"Artist", G_VARIANT_TYPE_STRING,
+				g_variant_new_string(""), NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"Album", G_VARIANT_TYPE_STRING,
+				g_variant_new_string(""), NULL);
+			dbus_test_dbus_mock_object_add_property(mock, soundobj,
+				"ArtUrl", G_VARIANT_TYPE_STRING,
+				g_variant_new_string(""), NULL);
+
+			dbus_test_service_add_task(service, DBUS_TEST_TASK(mock));
 			dbus_test_service_start_tasks(service);
 
 			g_setenv("DBUS_SYSTEM_BUS_ADDRESS", g_getenv("DBUS_SESSION_BUS_ADDRESS"), TRUE);
@@ -53,11 +107,12 @@ class AccountsServiceUserTest : public ::testing::Test
 		virtual void TearDown() {
 			/* These are the things that libaccountservice0 doesn't clean up :-( */
 			g_object_unref(act_user_manager_get_default());
-			g_object_unref(system);
-			g_object_unref(system);
-			g_object_unref(system);
+			for (int i = 0; i < 11; i++) {
+				g_object_unref(system);
+			}
 			/* End shitty untested library cleanup */
 
+			g_clear_object(&mock);
 			g_clear_object(&service);
 
 			g_object_unref(session);
