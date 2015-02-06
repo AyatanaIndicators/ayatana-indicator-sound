6ac4cc8173c25b12cf8c18869f7d1e142c28ad44
Ted Gould <ted@gould.cx>
2015-02-06 08:16:34 -0600

    Add in capabilities support

--- tests/notifications-mock.h
+++ tests/notifications-mock.h
@@ -24,15 +24,39 @@ class NotificationsMock
 		DbusTestDbusMock * mock = nullptr;
 
 	public:
-		NotificationsMock () {
+		NotificationsMock (std::vector<std::string> capabilities = {"body", "body-markup", "icon-static", "image/svg+xml", "x-canonical-private-synchronous", "x-canonical-append", "x-canonical-private-icon-only", "x-canonical-truncation", "private-synchronous", "append", "private-icon-only", "truncation"}) {
 			mock = dbus_test_dbus_mock_new("org.freedesktop.Notifications");
 			dbus_test_task_set_bus(DBUS_TEST_TASK(mock), DBUS_TEST_SERVICE_BUS_SESSION);
-			dbus_test_task_set_name(DBUS_TEST_TASK(mock), "Notifications");
+			dbus_test_task_set_name(DBUS_TEST_TASK(mock), "Notify");
+
+			DbusTestDbusMockObject * baseobj =dbus_test_dbus_mock_get_object(mock, "/org/freedesktop/Notifications", "org.freedesktop.Notifications", NULL);
+
+			std::string capspython("ret = ");
+			capspython += vector2py(capabilities);
+			dbus_test_dbus_mock_object_add_method(mock, baseobj,
+				"GetCapabilities", NULL, G_VARIANT_TYPE("as"),
+				capspython.c_str(), NULL);
 		}
 
 		~NotificationsMock () {
 			g_debug("Destroying the Accounts Service Mock");
 			g_clear_object(&mock);
+		}
+
+		std::string vector2py (std::vector<std::string> vect) {
+			std::string retval("[ ");
+
+			std::for_each(vect.begin(), vect.end() - 1, [&retval](std::string entry) {
+				retval += "'";
+				retval += entry;
+				retval += "', ";
+			});
+
+			retval += "'";
+			retval += *(vect.end() - 1);
+			retval += "']";
+
+			return retval;
 		}
 
 		operator std::shared_ptr<DbusTestTask> () {
