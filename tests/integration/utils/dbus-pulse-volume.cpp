2c16abc48dcd11201acc393970fd2629ded1294e
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2021-11-19 11:44:48 +0100

    tests/*: Use one spelling for "Canonical Ltd." in copyright headers.
     Some license headers use "Canonical, Ltd." while others use "Canonical
     Ltd." as copyright holder. The correct form is the latter, so this
     change adjusts the spelling accordingly.
    
     This unification helps with generating debian/copyright files via
     scripts.

--- tests/integration/utils/dbus-pulse-volume.cpp
+++ tests/integration/utils/dbus-pulse-volume.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Canonical, Ltd.
+ * Copyright (C) 2015 Canonical Ltd.
  * Copyright (C) 2021 Robert Tari
  *
  * This program is free software: you can redistribute it and/or modify it
